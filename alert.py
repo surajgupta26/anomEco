a2de34f6fe0b12e2ddf76d6771867ed3b98d86bb
Sohan DeSarkar <sohan.desarkar@uber.com>
2018-07-10 17:33:46 +0530

    Abstracted alert from alertSystem and added city_to_emails functionality

--- alert.py
+++ alert.py
@@ -1,49 +1,23 @@
-import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-class AlertSystem:
+class Alert:
 
-    def __init__(self, username='anomEco2018@gmail.com', password='anomEco@123'):
-        self.username='anomEco2018@gmail.com'
-        self.password='anomEco@123'
-        self.setDefaultMessage()
+    def __init__(self, datestring, location, fields, field_notifications):
+        self.datestring=datestring
+        self.location=location
+        self.fields=fields
+        self.field_notifications=field_notifications
+        self.anomaly_count=len(fields)
 
-    def startServer(self):
-        self.server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-        self.server.starttls()
-        self.server.login(self.username, self.password)
+    def getMessage(self):
+        message=MIMEMultipart()
+        message['Subject']='Anomlies in '+self.location+' on '+self.datestring
+        body='Location: '+self.location+'\nDate: '+self.datestring+'\nList of anomalies:\n'
+        for i in range(self.anomaly_count):
+            body+='\t'+self.fields[i]+': '+self.field_notifications[i]+'\n'
+        message.attach(MIMEText(body, 'plain'))
+        return message
 
-    def stopServer(self):
-        self.server.quit()
-
-    def setDefaultMessage(self, subject=None, body=None):
-        self.default_message=MIMEMultipart()
-        if subject is None:
-            subject='Alert'
-        self.default_message['Subject']=subject
-        if body is None:
-            body='Anomaly detected!'
-        self.default_message.attach(MIMEText(body, 'plain'))
-
-    def getDefaultMessage(self):
-        return self.default_message
-
-    def sendAlert(self, to, message=None, _from=None):
-        if _from is None:
-            _from = self.username
-        if message is None:
-            message=self.getDefaultMessage()
-            message['From']=_from
-            message['To']=to
-            text=message.as_string()
-        else:
-            text=message
-
-        self.server.sendmail(_from, to, text)
-
-if __name__=='__main__':
-    als=AlertSystem()
-    als.startServer()
-    als.sendAlert(to=als.username)
-    als.stopServer()
+    def getAnomalyCount(self):
+        return self.anomaly_count
--- alertSystem.py
+++ alertSystem.py
@@ -3,14 +3,19 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import cPickle
 import os.path
+from alert import Alert
+
+def saveObject(filename, obj):
+    f=open(filename,'w')
+    cPickle.dump(obj,f)
+    f.close()
 
 class AlertSystem:
 
     def __init__(self, username='anomEco2018@gmail.com', password='anomEco@123'):
         self.username='anomEco2018@gmail.com'
         self.password='anomEco@123'
-        self.setDefaultMessage()
-        self.loadCityToEmails()
+        self.loadLocationToEmails()
 
     def startServer(self):
         self.server = smtplib.SMTP(host='smtp.gmail.com', port=587)
@@ -20,41 +25,41 @@ class AlertSystem:
     def stopServer(self):
         self.server.quit()
 
-    def loadCityToEmails(self, path='city_to_emails.p'):
+    def loadLocationToEmails(self, path='location_to_emails.p'):
         if os.path.exists(path):
             f=open(path,'r')
-            self.city_to_emails=cPickle.load(f)
+            self.location_to_emails=cPickle.load(f)
             f.close()
         else:
-            self.city_to_emails={}
+            self.location_to_emails={}
 
-    def setDefaultMessage(self, subject=None, body=None):
-        self.default_message=MIMEMultipart()
-        if subject is None:
-            subject='Alert'
-        self.default_message['Subject']=subject
-        if body is None:
-            body='Anomaly detected!'
-        self.default_message.attach(MIMEText(body, 'plain'))
+    def addRecipient(self, location, recipient_email):
+        if location not in self.location_to_emails:
+            self.location_to_emails[location]=set()
+        self.location_to_emails[location].add(recipient_email)
+        saveObject('location_to_emails.p',self.location_to_emails)
 
-    def getDefaultMessage(self):
-        return self.default_message
+    def removeRecipient(self, location, recipient_email):
+        if recipient_email in self.location_to_emails[location]:
+            self.remove(recipient_email)
+            saveObject('location_to_emails.p',location_to_emails)
 
-    def sendAlert(self, to, message=None, _from=None):
-        if _from is None:
-            _from = self.username
-        if message is None:
-            message=self.getDefaultMessage()
-            message['From']=_from
-            message['To']=to
-            text=message.as_string()
-        else:
-            text=message
+    def getMessage(self, alert):
+        message=alert.getMessage()
+        message['From']=self.username
+        message['To']=','.join(list(self.location_to_emails[alert.location]))
+        return message
 
-        self.server.sendmail(_from, to, text)
+    def sendAlert(self, alert):
+        message=self.getMessage(alert)
+        if len(message['To'])==0:
+            return
+        self.server.sendmail(self.username, message['To'], message.as_string())
 
 if __name__=='__main__':
     als=AlertSystem()
+    als.addRecipient(location='Hyderabad',recipient_email=als.username)
     als.startServer()
-    als.sendAlert(to=als.username)
+    alert=Alert(datestring='10/06/2018',location='Hyderabad',fields=['fares'],field_notifications=['Total fares is going down!'])
+    als.sendAlert(alert)
     als.stopServer()
