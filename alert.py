d0df7043d21f8fc5dfaff7e59a08c76bacfbee1f
Sohan DeSarkar <sohan.desarkar@uber.com>
2018-07-10 14:43:29 +0530

    Added default email template

--- alert.py
+++ alert.py
@@ -1,11 +1,13 @@
 import smtplib
-# set up the SMTP server
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 class AlertSystem:
 
     def __init__(self, username='anomEco2018@gmail.com', password='anomEco@123'):
         self.username='anomEco2018@gmail.com'
         self.password='anomEco@123'
+        self.setDefaultMessage()
 
     def startServer(self):
         self.server = smtplib.SMTP(host='smtp.gmail.com', port=587)
@@ -15,13 +17,33 @@ class AlertSystem:
     def stopServer(self):
         self.server.quit()
 
-    def sendAlert(self, to, msg, _from=None):
+    def setDefaultMessage(self, subject=None, body=None):
+        self.default_message=MIMEMultipart()
+        if subject is None:
+            subject='Alert'
+        self.default_message['Subject']=subject
+        if body is None:
+            body='Anomaly detected!'
+        self.default_message.attach(MIMEText(body, 'plain'))
+
+    def getDefaultMessage(self):
+        return self.default_message
+
+    def sendAlert(self, to, message=None, _from=None):
         if _from is None:
             _from = self.username
-        self.server.sendmail(_from, to, msg)
+        if message is None:
+            message=self.getDefaultMessage()
+            message['From']=_from
+            message['To']=to
+            text=message.as_string()
+        else:
+            text=message
+
+        self.server.sendmail(_from, to, text)
 
 if __name__=='__main__':
     als=AlertSystem()
     als.startServer()
-    als.sendAlert(to=als.username,msg="This is Test!")
+    als.sendAlert(to=als.username)
     als.stopServer()
