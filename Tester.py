dfaf0b5aea75089ad447663b002b0b4f9e27f38f
Sohan DeSarkar <sohan.desarkar@uber.com>
2018-07-12 15:40:42 +0530

    Changed Tester

--- Tester.py
+++ Tester.py
@@ -45,11 +45,10 @@ class RunThread(threading.Thread):
         yab_command="yab -s ueconomics -m Ueconomics::getTripEconomics -t /usr/share/uber-idl/code.uber.internal/finance/ueconomics/ueconomics.thrift --caller yab-sohan.desarkar --request '"
         yab_command+=json.dumps(yab_template)
         yab_command+="' --header 'x-uber-uuid:b091e5dc-954b-4603-b224-531522ef19a3' --header 'x-uber-source:studio' --timeout=30000 -p 127.0.0.1:5437"
-        pipe_command=' > data/'+self.location
+        # pipe_command=' > data/'+self.location
         return yab_command+pipe_command
 
     def run(self):
-        print self.getYabCommand()
         os.system(self.getYabCommand())
         # stdoutdata=p.communicate()[0]
         # new_data=json.loads(stdoutdata)
